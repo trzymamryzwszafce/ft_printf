52a728c4c803469b47b2f4ca98999994a97020f4
trzymamryzwszafce <szymonmadeja523@gmail.com>
2025-01-08 21:36:11 +0100

    sdf

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,40 @@
 /*   By: szmadeja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:22:31 by szmadeja          #+#    #+#             */
-/*   Updated: 2025/01/07 20:17:21 by szmadeja         ###   ########.fr       */
+/*   Updated: 2025/01/08 15:00:24 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int ft_format(char letter, va_list ap)
+{
+    int i;
+
+    i = 0;
+    if (letter == 'c')
+        i += ft_char(va_arg(ap, int));
+    else if (letter == 's')
+        i += ft_str(va_arg(ap, int));
+    else if (letter == 'p')
+        i += ft_ptr(va_arg(ap, int));
+    else if (letter == 'd')
+        i += ft_dec(va_arg(ap,int));
+    else if (letter == 'i')
+        i += ft_int(va_arg(ap, int));
+    else if (letter == 'u')
+        i += ft_udec(va_arg(ap, int));
+    else if (letter == 'x')
+        i += ft_lhex(va_arg(ap, int));
+    else if (letter == 'X')
+        i += ft_uhex(va_arg(ap, int));
+    else if (letter == '%')
+        i += ft_per(va_arg(ap, int));
+    else
+        i += write(1, &letter, 1);
+    return (i);
+
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -19,7 +47,7 @@ int	ft_printf(const char *format, ...)
 	int	i;
 
 	va_start(ap, format);
-	count = 0;
+	i = 0;
 	while (*format)
 	{
 		if (*format == '%')
@@ -30,3 +58,4 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(ap);
 	return (i);
+}
